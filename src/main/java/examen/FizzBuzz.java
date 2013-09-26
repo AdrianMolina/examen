a344887d9f1914f5a7f540634b96dfbfece7b768
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-25 21:19:45 -0400

    pruebas terminadas

--- src/main/java/examen/FizzBuzz.java
+++ src/main/java/examen/FizzBuzz.java
@@ -3,8 +3,12 @@ package examen;
 public class FizzBuzz {
 
 	public String genera(int i) {
-		if (i == 3)
+		if ((i % 3 == 0) && (i % 5 == 0))
+			return "FizzBuzz";
+		if (i % 3 == 0)
 			return "Fizz";
+		if (i % 5 == 0)
+			return "Buzz";
 		return String.valueOf(i);
 	}
 
--- src/test/java/examen/FizzBuzzTest.java
+++ src/test/java/examen/FizzBuzzTest.java
@@ -2,20 +2,49 @@ package examen;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
-
+	private FizzBuzz fb;
+	
+	@Before
+	public void setup(){
+		fb = new FizzBuzz();
+	}
+	
 	@Test
 	public void generaFizzCuandoNumeroEsTres() {
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("Fizz", fb.genera(3));
 	}
 	
 	@Test
 	public void generaElMismoNumeroSiNoEsTres(){
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("4", fb.genera(4));
 	}
-
+	
+	@Test
+	public void generarFizzSiElNumeroEsMultiploDeTres(){
+		assertEquals("Fizz", fb.genera(6));
+	}
+	
+	@Test
+	public void generarBuzzSiElNumeroEsCinco(){
+		assertEquals("Buzz", fb.genera(5));
+	}
+	
+	@Test
+	public void generarBuzzSiElNumeroEsMultiploDeCinco(){
+		assertEquals("Buzz", fb.genera(10));
+	}
+	
+	@Test
+	public void generarFizzBuzzSiElNumeroEsQuince(){
+		assertEquals("FizzBuzz", fb.genera(15));
+	}
+	
+	@Test
+	public void generarFizzBuzzSiElNumeroEsMultiploDeTresYCinco(){
+		assertEquals("FizzBuzz", fb.genera(30));
+	}
 }
