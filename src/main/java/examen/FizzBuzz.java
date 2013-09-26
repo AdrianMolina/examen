e5c8fecdb8bc5de4c680458df81bed07f447a5ee
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-25 21:35:56 -0400

    refactorizado fizzbuzz

--- src/main/java/examen/FizzBuzz.java
+++ src/main/java/examen/FizzBuzz.java
@@ -2,14 +2,23 @@ package examen;
 
 public class FizzBuzz {
 
+	private static final int _BUZZ = 5;
+	private static final int _FIZZ = 3;
+	private static final String _MENSAJE_FIZZ = "Fizz";
+	private static final String _MENSAJE_BUZZ = "Buzz";
+	private static final String _MENSAJE_FIZZBUZZ = "FizzBuzz";
+
 	public String genera(int i) {
-		if ((i % 3 == 0) && (i % 5 == 0))
-			return "FizzBuzz";
-		if (i % 3 == 0)
-			return "Fizz";
-		if (i % 5 == 0)
-			return "Buzz";
+		if (esMultiploDe(i, _FIZZ) && esMultiploDe(i, _BUZZ))
+			return _MENSAJE_FIZZBUZZ;
+		if (esMultiploDe(i, _FIZZ))
+			return _MENSAJE_FIZZ;
+		if (esMultiploDe(i, _BUZZ))
+			return _MENSAJE_BUZZ;
 		return String.valueOf(i);
 	}
 
+	private boolean esMultiploDe(int numero, int multiplo) {
+		return numero % multiplo == 0;
+	}
 }
